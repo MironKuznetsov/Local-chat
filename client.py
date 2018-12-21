a4585cdeb67a701e0457f1112e550a57a2dd7de8
Ulia <demidova.ua@phystech.edu>
2018-12-21 22:11:28 +0300

    client

---
+++ client.py
@@ -0,0 +1,68 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
+
+
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg_list.insert(tkinter.END, msg)
+        except OSError:
+            break
+
+
+def send(event=None):
+    msg = my_msg.get()
+    my_msg.set("")
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "/q":
+        client_socket.close()
+        top.quit()
+
+
+def on_closing(event=None):
+    my_msg.set("/q")
+    send()
+
+
+top = tkinter.Tk()
+top.title("MIPT chat")
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()
+my_msg.set("Type here")
+scrollbar_y = tkinter.Scrollbar(messages_frame)
+
+msg_list = tkinter.Listbox(messages_frame, height=20, width=70, yscrollcommand=scrollbar_y.set)
+scrollbar_y.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+HOST = input('Enter host: ')
+# PORT = input('Enter port: ')
+# if not PORT:
+#     PORT = 33000
+# else:
+#     PORT = int(PORT)
+PORT = 33000
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()
